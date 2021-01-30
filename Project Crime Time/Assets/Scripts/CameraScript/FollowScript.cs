9aa6c2acccd6be2e6e1f5d3726199b1d25c65997
felix34ever1 <felix360onea@gmail.com>
2021-01-30 11:52:55 +0000

    Added game objects
    -Added the player, camera and ground.
    
    -Gave them incredibly temp textures.
    
    -Made scripts for movement.

---
+++ Project Crime Time/Assets/Scripts/CameraScript/FollowScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowScript : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    public Transform CameraTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float camerax = PlayerTransform.position.x;
+        float cameray = PlayerTransform.position.y;
+        float cameraz = PlayerTransform.position.z-10;
+        CameraTransform.position = new Vector3(camerax,cameray,cameraz);
+    }
+}
---
+++ Project Crime Time/Assets/Scripts/PlayerScripts/MovementScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public Transform tf;
+
+    public float sidewaysForce = 1000f;
+    public float upwardsForce = 1000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tf.rotation = new Quaternion(0, 0, 0, 0 ); //This makes the rotation always that< and so it stays upright.
+        if (Input.GetKey("d"))//Adds movement right
+        {
+            rb.AddForce(new Vector2(sidewaysForce*Time.deltaTime,0f));
+        }
+        if (Input.GetKey("a"))//Adds movement left
+        {
+            rb.AddForce(new Vector2(-sidewaysForce * Time.deltaTime, 0f));
+        }
+
+ 
+    }
+    //Allows for jumping as long as the player is colliding.
+    //Need to make it so that it has to only be colliding on the floor so it can't wall jump or stuff. (Unless we want to do that)
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(new Vector2(0f, upwardsForce * Time.deltaTime));
+        }
+    }
+
+}
