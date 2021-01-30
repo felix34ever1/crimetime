3adedc87262a3673e21c06bc2509ff927bbc5e27
felix34ever1 <felix360onea@gmail.com>
2021-01-30 15:10:05 +0000

    Literally did nothing
    I changed nothing

--- Project Crime Time/Assets/Bullet.cs
+++ Project Crime Time/Assets/Bullet.cs
@@ -7,12 +7,25 @@ public class Bullet : MonoBehaviour
     public float speed = 10f;
     public Rigidbody2D rb;
     public GameObject bullet;
-
+    public int bulletTime = 5;
+    private int x = 0;
     // Start is called before the first frame update
     void Start()
     {
         rb.velocity = transform.right * speed;
     }
+
+//    private void Update()
+//    {
+//        while (x != bulletTime)
+//        {
+//            x = x + 1;
+//        } 
+//        else
+//        {
+//            Destroy(bullet);
+//        }
+//    }
 
     private void OnTriggerEnter2D(Collider2D hitInfo)
     {
