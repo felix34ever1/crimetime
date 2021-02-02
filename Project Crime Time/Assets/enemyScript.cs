b5ff8caf10e8fccd8e98e560dd47bbfa71c28028
felix34ever1 <felix360onea@gmail.com>
2021-02-02 22:37:14 +0000

    Started Working on detection.
    -Started working on enemy AI and player detection.
    -This build is broken and not runnable

---
+++ Project Crime Time/Assets/enemyScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour
+{
+    public GameObject go; // The game object of itself.
+    public GameObject player;// the gameobject of the player.
+    public int range; // the range that the detection of the enemy will be
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    void detect()
+    {
+        RaycastHit2D hit2D;//will once correctly created, shoot out raycast to detect player!
+        if (Physics2D.Raycast(go.transform.position, go.transform.transform.right, out hit2D, range))
+        {
+            Debug.Log(hit2D.transform.name);
+        }
+    }
+}
